9e279bf87a7c6c8a9e38afca04015bdeacde775f
Eduardo Vizcaino Granados <e.vizcaino@solesgps.com>
2022-08-12 08:39:03 -0500

    Estandarizando Modelos

---
+++ models/tc_devices.py
@@ -0,0 +1,27 @@
+
+
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+import datetime, time
+import requests, json
+import random
+import base64
+from dateutil.relativedelta import relativedelta
+from odoo import api, fields, models, _
+import pytz
+class tc_devices(models.Model):
+    _name = "tc_devices"
+    _description = 'traccar devices'
+    _order = "name DESC"
+        
+    name                                        = fields.Char('Name', size=128)
+    uniqueid                                    = fields.Char('IMEI', size=128)
+    icc                                         = fields.Char('ICC', size=30)
+    phone                                       = fields.Char('Phone', size=128)
+    model                                       = fields.Char('Model', size=128)
+    lastupdate                                  = fields.Datetime('Lastupdate')
+    disabled                                    = fields.Boolean('Disable', default=False)
+    telcel                                      = fields.Boolean('Telcel', default=True)
+    signal                                      = fields.Boolean('Good signal', default=True)
+    company_ids                                 = fields.Many2many('res.company', 'tcdevices_res_company_rel', 'user_id', 'cid', string='Companies', default=lambda self: self.env.user.company_id)
+    

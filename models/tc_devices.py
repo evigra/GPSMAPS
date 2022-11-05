383e8c97c96e58459d3ad3e0c8f01e10724e1b92
Eduardo Vizcaino Granados <e.vizcaino@solesgps.com>
2022-11-04 23:06:08 -0600

    Agregar _sql_constraints unique y validacion en metodo crear device

--- models/tc_devices.py
+++ models/tc_devices.py
@@ -13,7 +13,7 @@ class tc_devices(models.Model):
     _order = "name DESC"
         
     name                                        = fields.Char('Name', size=128)
-    uniqueid                                    = fields.Char('IMEI', size=128)
+    uniqueid                                    = fields.Char('IMEI', size=128, required=True)
     icc                                         = fields.Char('ICC', size=30)
     phone                                       = fields.Char('Phone', size=128)
     model                                       = fields.Char('Model', size=128)
@@ -23,15 +23,20 @@ class tc_devices(models.Model):
     signal                                      = fields.Boolean('Good signal', default=True)
     company_ids                                 = fields.Many2many('res.company', 'tcdevices_res_company_rel', 'user_id', 'cid', string='Companies', default=lambda self: self.env.user.company_id)
 
-    """
-    @api.multi
-    def _create(self, vals):
-        print("########## CREAR GPS")
-        print("########## vals", vals)
+    _sql_constraints = [
+        ('uniqueid_uniq', 'unique(uniqueid)', "The imei of the GPS device already exists"),
+    ]    
+
+    @api.model
+    def create(self, vals):
+        if "uniqueid" in vals:            
+            devices_arg = [('uniqueid', '=', vals["uniqueid"])]
+            return self.search(devices_arg)        
+        else:
+            return  super(tc_devices, self).create(vals)
     
-    @api.multi
-    def _write(self, vals):  
-        print("########## WRITE GPS")
-        print("########## vals", vals)
-    """    
-    
+"""    
+    @api.one
+    def write(self, vals):  
+        return super(tc_devices, self).write(vals)
+"""
